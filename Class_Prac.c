8dfff4b490b99ced039f60c99bea12e8c1be5c37
uddinmdmine28-crypto <uddinmdmine28@gmail.com>
2026-09-23 19:39:45 +0900

    Add files via upload

---
+++ Class_Prac.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    scanf("%d %d",&x, &y);
+    
+
+    printf("Sum = %d\n",x+y);
+    printf("Sub = %d\n",x-y);
+    printf("Product = %d\n",x*y);
+    printf("Divide = %d\n",x/y);
+    printf("Remainder = %d\n",x%y);
+
+    return 0;
+}
